dc563711bb1d68176f5946fa18b1040f63ea2c33
Aswin Kandula <31377402+kandula-aswin@users.noreply.github.com>
2018-03-27 08:40:22 +1100

    New TreeActionCreator with R and W backing services

---
+++ corpdir-srv-web-ui/src/main/java/com/github/bordertech/corpdir/web/ui/flux/actioncreator/temp/CorpCrudTreeActionCreatorTemp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.bordertech.corpdir.web.ui.flux.actioncreator.temp;
+
+import com.github.bordertech.corpdir.api.common.ApiTreeable;
+import com.github.bordertech.corpdir.web.ui.flux.dataapi.temp.CorpCrudTreeDataApiTemp;
+import com.github.bordertech.flux.crud.actioncreator.DataApiCrudTreeActionCreator;
+
+/**
+ * Corp CRUD Tree Action Creator with defined types.
+ *
+ * @param <T> the API type
+ * @param <D> the backing data API
+ * 
+ * @author Jonathan Austin
+ * @author Aswin Kandula
+ */
+public interface CorpCrudTreeActionCreatorTemp<T extends ApiTreeable, D extends CorpCrudTreeDataApiTemp<T, ?, ?>> extends CorpCrudActionCreatorTemp<T, D>, DataApiCrudTreeActionCreator<String, T, D> {
+    
+}
