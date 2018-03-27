a8967d8449181d7e14c0828b0ccebc1f58e1743f
Aswin Kandula <31377402+kandula-aswin@users.noreply.github.com>
2018-03-27 11:42:01 +1100

    Deprecated old PositionType service and it’s implementation

--- corpdir-srv-api/src/main/java/com/github/bordertech/corpdir/api/v1/PositionTypeService.java
+++ corpdir-srv-api/src/main/java/com/github/bordertech/corpdir/api/v1/PositionTypeService.java
@@ -4,6 +4,7 @@ import com.github.bordertech.corpdir.api.response.DataResponse;
 import com.github.bordertech.corpdir.api.service.BasicKeyIdService;
 import com.github.bordertech.corpdir.api.v1.model.Position;
 import com.github.bordertech.corpdir.api.v1.model.PositionType;
+import com.github.bordertech.corpdir.readonly.api.v1.PositionTypeReadOnlyService;
 import java.util.List;
 
 /**
@@ -11,9 +12,18 @@ import java.util.List;
  *
  * @author Jonathan Austin
  * @since 1.0.0
+ * @deprecated split into read and write
  */
+@Deprecated
 public interface PositionTypeService extends BasicKeyIdService<PositionType> {
 
+	/**
+	 * 
+	 * @param keyId
+	 * @return 
+	 * @deprecated use {@link PositionTypeReadOnlyService#getPositions(java.lang.String) } instead.
+	 */
+	@Deprecated
 	DataResponse<List<Position>> getPositions(final String keyId);
 
 }
